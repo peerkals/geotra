a69a6e3638d83f8bb09eaaefcff2b8e28e3ba77d
ernestopos <ernestopos@gmail.com>
2022-09-02 04:53:41 -0500

    deploy heroku

--- geotra/server.js
+++ geotra/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/geotra'));
+app.use(express.static(__dirname + '/dist/geotrabaq'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/geotra/index.html'));});
+res.sendFile(path.join(__dirname + '/dist/geotrabaq/index.html'));});
 app.listen(process.env.PORT || 8080);
