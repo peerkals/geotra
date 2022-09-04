84f7f7ae668f7cec5468cdc09c05908ba1a39ecc
ernestopos <ernestopos@gmail.com>
2022-09-03 22:57:33 -0500

    prueba

--- geotra/server.js
+++ geotra/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/geotrabaq'));
+app.use(express.static(__dirname + '/dist/adminpro'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/geotrabaq/index.html'));});
+res.sendFile(path.join(__dirname + '/dist/adminpro/index.html'));});
 app.listen(process.env.PORT || 8080);
