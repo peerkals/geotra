0421b7d73e4f6b31eecdb019218dc9e5fd7a8400
ernestopos <ernestopos@gmail.com>
2022-09-04 11:17:09 -0500

    initial heroku deploy

--- geotra/server.js
+++ geotra/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(__dirname + '/dist/adminpro'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/adminpro/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/adminpro/index.html'));
+});
+app.listen(process.env.PORT || 5000);
