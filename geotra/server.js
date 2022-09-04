10920321680463bc6bf4afd3d179204aee4b3083
ernestopos <ernestopos@gmail.com>
2022-09-04 11:22:51 -0500

    Despliegue aplicacion

--- geotra/server.js
+++ geotra/server.js
@@ -6,4 +6,5 @@ app.use(express.static(__dirname + '/dist/adminpro'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/adminpro/index.html'));
 });
+
 app.listen(process.env.PORT || 5000);
